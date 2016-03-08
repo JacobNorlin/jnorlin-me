f263b8ce2152fa79b4b402ff4dc00ccbce374b4b
JacobNA <jacob.norlinandersson@gmail.com>
2016-03-08 01:12:46 +0100

    Test data for repoList

---
+++ src/data/testData.js
@@ -0,0 +1,11 @@
+var testData = [
+	{link: "lmao.com",
+	title: "ayy",
+	summary: "ayy lmao"
+	},
+	{link: 'https://www.youtube.com/watch?v=Qg3XOfioapI',
+	title: "Kolmogorov Music",
+	summary: "About complexity of data and how that can be applied to music"}
+]
+
+export {testData};
