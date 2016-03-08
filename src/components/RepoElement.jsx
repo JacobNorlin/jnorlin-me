cd2fc0077232697d7c3896bbe0c55bfab2685231
JacobNA <jacob.norlinandersson@gmail.com>
2016-03-08 02:19:09 +0100

    Compnent for repo elements

--- src/components/RepoElement.jsx
+++ src/components/RepoElement.jsx
@@ -1,9 +1,24 @@
+"use strict"
 import React from 'react';
 
-export default class RepoElement extends React.Component {
+export default class ProjectElem extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			link: props.link,
+			summary: props.summary,
+			title: props.title
+		}
 	}
 
-	render();
+	render(){
+		return <div>
+					<h2>{this.state.title}</h2>
+					<div className="elemContainer">
+						<a>{this.state.link}</a>
+						<h4>Summary</h4>
+						<p>{this.state.summary}</p>
+					</div>
+				</div>
+	}
 }
