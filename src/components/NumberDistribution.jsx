3c23317b468f35a0ff0ff62c1103b0ed258418d4
JacobNA <jacob.norlinandersson@gmail.com>
2016-03-15 23:45:10 +0100

    Disabled start button while generating numbers

--- src/components/NumberDistribution.jsx
+++ src/components/NumberDistribution.jsx
@@ -2,7 +2,7 @@
 
 import d3 from 'd3';
 import NumberGenerator from './PrimeGenerator.js';
-import {BarChart} from 'react-d3-components';
+import {BarChart, LineChart} from 'react-d3-components';
 import React from 'react';
 import {Row, Col, Grid, PageHeader, Button, Input} from 'react-bootstrap';
 import _ from 'lodash';
@@ -14,19 +14,23 @@ export default class NumberDistribution extends React.Component {
 		super(props);
 		let numberGenerator = new NumberGenerator();
 
+		this.initialState = [{label:"lol", values:[{x:"0", y:0}, {x:"1", y:0}, {x:"2", y:0}, {x:"3", y:0} , {x:"4", y:0}, {x:"5", y:0}, {x:"6", y:0}, {x:"7", y:0}, {x:"8", y:0}, {x:"9", y:0}]}],
 		this.state =  {
 			data: [{label:"lol", values:[{x:"0", y:0}, {x:"1", y:0}, {x:"2", y:0}, {x:"3", y:0} , {x:"4", y:0}, {x:"5", y:0}, {x:"6", y:0}, {x:"7", y:0}, {x:"8", y:0}, {x:"9", y:0}]}],
 			numberGenerator: numberGenerator,
 			startValue: 1,
-			stop: false
+			stop: false,
+			running: false
 		}
 	}
 
 	startGen(event){
 		let gen = this.state.numberGenerator.getPrimeLastDigitDistributionStream(1, this.state.startValue);
-		this.setState({stop:false});
+		this.setState({stop:false, running: true});
 		gen.takeWhile(this.shouldStop.bind(this))
 		.subscribe(d => {
+
+			console.log(d);
 			let old = this.state.data;
 			for(let i = 0; i < 10; i++){
 				old[0].values[i].y = d[i];
@@ -39,12 +43,11 @@ export default class NumberDistribution extends React.Component {
 	}
 
 	shouldStop(){
-		console.log(this.state);
 		return !this.state.stop;
 	}
 
 	stopGen(){
-		this.setState({stop:true})
+		this.setState({stop:true, running: false, data: this.initialState})
 	}
 
 	componentWillUnmount(){
@@ -66,8 +69,8 @@ export default class NumberDistribution extends React.Component {
 		label="Starting number"
 		onChange={(e) => this.setState({startValue: e.target.value})}
 		/>
-		<Button bsStyle="primary" onClick={this.startGen.bind(this)}>Start</Button>
-		<Button bsStyle="primary" onClick={this.stopGen.bind(this)}>Stop</Button>
+		<Button bsStyle="primary" disabled={this.state.running} onClick={this.startGen.bind(this)}>Start</Button>
+		<Button bsStyle="primary" disabled={!this.state.running} onClick={this.stopGen.bind(this)}>Stop</Button>
 		<BarChart
 		data={this.state.data}
 		width={400}
